1f61722b94891595db10e29ebf020033a2542b6e
Joe-noh <goflb.jh@gmail.com>
2014-12-03 05:42:47 +0900

    add defimpl placeholder

---
+++ lib/colorful_pp/formatter.ex
@@ -0,0 +1,101 @@
+defprotocol ColorfulPP.Formatter do
+  @fallback_to_any true
+  def format(obj)
+end
+
+defimpl ColorfulPP.Formatter, for: Atom do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: BitString do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: Integer do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: Float do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: Range do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: Regex do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: Function do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: PID do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: Reference do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: Tuple do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: List do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: Map do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: HashSet do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: HashDict do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: Stream do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
+defimpl ColorfulPP.Formatter, for: Any do
+  def format(obj) do
+    IO.inspect obj
+  end
+end
+
