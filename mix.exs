a630d693a600aa4b7e40f240ffe42fdfeafa8685
Joe-noh <goflb.jh@gmail.com>
2014-12-03 13:17:11 +0900

    add colorizer module

--- mix.exs
+++ mix.exs
@@ -25,6 +25,8 @@ defmodule ColorfulPP.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:colorful, "~> 0.5"}
+    ]
   end
 end
--- test/formatter_test.exs
+++ test/formatter_test.exs
@@ -1,30 +1,27 @@
 defmodule FormatterTest do
   use ExUnit.Case
-  import ExUnit.CaptureIO
 
   import ColorfulPP.Formatter
 
   defstruct a: nil, b: nil
 
   test "protocol implemented" do
-    capture_io fn ->
-      format :atom
-      format "bitstring"
-      format 1
-      format 1.0
-      format 1..2
-      format ~r/regex/
-      format fn -> :function end
-      format self
-      format make_ref
-      format {:tuple}
-      format ["list"]
-      format 'char_list'
-      format %{"type" => "map"}
-      format HashSet.new
-      format HashDict.new
-      format Stream.map(1..2, &(&1))
-      format %__MODULE__{}
-    end
+    format :atom
+    format "bitstring"
+    format 1
+    format 1.0
+    format 1..2
+    format ~r/regex/
+    format fn -> :function end
+    format self
+    format make_ref
+    format {:tuple}
+    format ["list"]
+    format 'char_list'
+    format %{"type" => "map"}
+    format HashSet.new
+    format HashDict.new
+    format Stream.map(1..2, &(&1))
+    format %__MODULE__{}
   end
 end
